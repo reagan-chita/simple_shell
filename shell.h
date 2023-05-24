c1412839e72fc425ddc80cd4f8d1a858e36c2827
Ackson Mutuma <78912538+Ackson94@users.noreply.github.com>
2023-05-24 16:33:19 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,12 +1,3 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
-
-/*
- * File: shell.h
- * Auth: Alex Yu
- *       Brennan D Baraban
- */
-
 #include <fcntl.h>
 #include <signal.h>
 #include <sys/types.h>
@@ -16,9 +7,6 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
-
-#define END_OF_FILE -2
-#define EXIT -3
 
 /* Global environemnt */
 extern char **environ;
@@ -136,6 +124,5 @@ void help_env(void);
 void help_setenv(void);
 void help_unsetenv(void);
 void help_history(void);
-
 int proc_file_commands(char *file_path, int *exe_ret);
-#endif /* _SHELL_H_ */
+
